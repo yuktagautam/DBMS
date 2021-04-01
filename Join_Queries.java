f028553d92e5cefba4c52ada0c6377b3e5fb2b4d
yuktagautam <44221585+yuktagautam@users.noreply.github.com>
2021-04-01 10:54:56 +0530

    Update Join_Queries.java

--- Join_Queries.java
+++ Join_Queries.java
@@ -15,9 +15,35 @@ select ename,dname from emp,dept where emp.deptno=dept.deptno;
 select ename,deptno,dname from emp,dept where emp.deptno=dept.deptno;
 //RIGHT
 select ename,emp.deptno,dept.deptno,dname from emp,dept where emp.deptno=dept.deptno;
-//--------------------------------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------------------INNER JOIN-------------------------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------INNER JOIN------------------------------------------------------------------------------------------------
 //isme do table kai beech mai sare  valid non null(matlb jsike corresponding koi non null value exist karti ho,vo sare print honge)
   empno ename  deptno             deptno dname Loction
   124  Ram   20                   30      Neha  delhi
   //in this case no data will be obtained ,because inner 20 kai link pai koi value avilable nhi hai
+  //query--->
+select ename,dname from emp,dept where emp.deptno=dept.deptno;  //actually a inner join
+select ename,dname from emp inner join dept on emp.deptno=dept.deptno;   //other method to write " , kai place pai join Type and where kai place pai on"
+-------------------------------------------------LEFT OUTER JOIN-----------------------------------------------------------------
+  //emp left outer dept
+  //left mai emp likha hai 
+  //right mai dept
+ //Join TYPE->left outer  -->matlb left mai jo hoga uske sare records whether their corresponding value present in other table or not 
+  empno ename  deptno             deptno dname Loction
+  124  Ram   20                   30      Neha  delhi
+  //is dept  table mai 20 deptno exist nhi karta but fir bhi ye Ram ka name print karega or dname kai place pai "-" sign a jayega
+  //query----->
+  select ename,dname from emp left outer join dept on emp.deptno=dept.deptno;
+select ename,dname from emp,dept where emp.deptno=dept.deptno(+)  -->agr sign right vale kai sath presnt ho toh matlb left ki sari records (left outer join)
+---------------------------------------------RIGHT OUTER JOIN--------------------------------------------------
+   //emp left outer dept
+  //left mai emp likha hai 
+  //right mai dept
+ //Join TYPE->right outer  -->matlb right mai jo hoga uske sare records prit honge whether their corresponding value present in other table or not 
+  empno ename  deptno             deptno dname Loction
+  124  Ram   20                   30      Neha  delhi
+  //is ename mai deptno 30 exist  nhi karta but fir bhi ye Neha ka name print karega or ename kai place pai "-" sign a jayega
+ //query----->
+select ename,dname from emp rightouter join dept on emp.deptno=dept.deptno;
+select ename,dname from emp,dept where emp.deptno(+)=dept.deptno -->agr sign left vale kai sath  sign present ho toh matlb right ki sari records (right outer join)
+ 
