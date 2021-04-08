415eaddd7f56dbf44844cb33a001947235ae6e97
yuktagautam <44221585+yuktagautam@users.noreply.github.com>
2021-04-08 12:24:54 +0530

    Update Normalization.java

--- Normalization.java
+++ Normalization.java
@@ -1,1 +1,4 @@
+//Decomposition must be in way that it must be lossless decompostion,we can't sacrifice this property ,if did it will not be consider as a valid decomposition
+//Functional dependencies  should be conserved,we can sacrifice this sometimes
 
+  
